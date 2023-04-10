3a8cb2c82c829e3b75ed7a7a457aed31e4b5dd27
Kevin Bravo <bravokevinto@gmail.com>
2023-04-09 23:11:21 -0400

    styles: add favicon

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head';
+import { ColorSchemeEnum } from 'next/dist/lib/metadata/types/metadata-types';
 import './globals.css'
 import PublicFooter from '@/components/footer/Public';
-
+import favicon from '../../../public/logo-proexcelencia-cap.png';
 export const metadata = {
   title: 'Proexcelencia',
   description: 'Sistema de Evaluacion del Becario',
+  icons: favicon.src,
 }
 
 export default function RootLayout({
@@ -15,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="es">
       <Head>
-        <link rel="icon" type="image/png" href='/logo-proexcelencia-cap.png' />
       </Head>
       <body>
         {children}
