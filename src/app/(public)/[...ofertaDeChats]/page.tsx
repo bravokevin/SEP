e88b27f2e664806f2b4c7ec8257ab08cef3a02b4
Kevin Bravo <bravokevinto@gmail.com>
2023-05-24 13:24:57 -0400

    feat: add page for holding chats of the month

---
+++ src/app/(public)/[...ofertaDeChats]/page.tsx
@@ -0,0 +1,371 @@
+import Image from 'next/image'
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='bg-gradient-to-br from-purple-950  to-slate-950 '>
+      <div className="container p-8 lg:space-y-0 lg:pt-12 min-h-screen ">
+        <div className="flex flex-col items-center gap-8">
+          <div>
+            <h1 className='font-semibold text-7xl text-center'>June Schedule</h1>
+          </div>
+          <div className="flex flex-col gap-4 justify-center items-center">
+            <h2 className='font-semibold text-5xl mb-4'>Chats Virtuales</h2>
+            <div className="flex flex-wrap justify-center items-center gap-6">
+
+              <div className='flex gap-4 bg-gradient-to-br from-purple-400  to-emerald-700 p-4 rounded-md w-[420px] items-center overflow-hidden '>
+                <div className='-mt-8'>
+                  <img src='https://i.imgur.com/FMP6aFy.png' width={100} height={100} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  < text className='font-bold text-xl' >
+                    Let's learn English
+                  </text >
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Dicatado por:
+                    </text>
+                    <text className='font-bold'>
+                      Jennipher Dolinsky
+                    </text>
+                  </span>
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Nivel:
+                    </text>
+                    <text className='font-bold'>
+                      Intermedio
+                    </text>
+                  </span>
+                  <span className=' flex gap-1 font-bold'>
+                    <text className=''>
+                      Sabado 12 de Junio
+                    </text>
+                    <text className=''>
+                      a las 7:00 pm
+                    </text>
+                  </span>
+                  <div className='absolute -ml-40'>
+                    <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path clipRule="evenodd" fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm0 8.625a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM15.375 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" />
+                    </svg>
+                  </div>
+                  <div className='h-6'>
+                    <button type="button" className="absolute w-[420px] -ml-[132px] text-white bg-purple-800  font-medium rounded-b-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Inscribirse</button>
+                  </div>
+                </div>
+              </div>
+              <div className='flex gap-4 bg-gradient-to-br from-purple-400  to-emerald-700 p-4 rounded-md w-[420px] items-center overflow-hidden '>
+                <div className='-mt-8'>
+                  <img src='https://i.imgur.com/FMP6aFy.png' width={100} height={100} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  < text className='font-bold text-xl' >
+                    Let's learn English
+                  </text >
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Dicatado por:
+                    </text>
+                    <text className='font-bold'>
+                      Jennipher Dolinsky
+                    </text>
+                  </span>
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Nivel:
+                    </text>
+                    <text className='font-bold'>
+                      Intermedio
+                    </text>
+                  </span>
+                  <span className=' flex gap-1 font-bold'>
+                    <text className=''>
+                      Sabado 12 de Junio
+                    </text>
+                    <text className=''>
+                      a las 7:00 pm
+                    </text>
+                  </span>
+                  <div className='absolute -ml-40'>
+                    <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path clipRule="evenodd" fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm0 8.625a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM15.375 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" />
+                    </svg>
+                  </div>
+                  <div className='h-6'>
+                    <button type="button" className="absolute w-[420px] -ml-[132px] text-white bg-purple-800  font-medium rounded-b-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Inscribirse</button>
+                  </div>
+                </div>
+              </div>
+              <div className='flex gap-4 bg-gradient-to-br from-purple-400  to-emerald-700 p-4 rounded-md w-[420px] items-center overflow-hidden '>
+                <div className='-mt-8'>
+                  <img src='https://i.imgur.com/FMP6aFy.png' width={100} height={100} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  < text className='font-bold text-xl' >
+                    Let's learn English
+                  </text >
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Dicatado por:
+                    </text>
+                    <text className='font-bold'>
+                      Jennipher Dolinsky
+                    </text>
+                  </span>
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Nivel:
+                    </text>
+                    <text className='font-bold'>
+                      Intermedio
+                    </text>
+                  </span>
+                  <span className=' flex gap-1 font-bold'>
+                    <text className=''>
+                      Sabado 12 de Junio
+                    </text>
+                    <text className=''>
+                      a las 7:00 pm
+                    </text>
+                  </span>
+                  <div className='absolute -ml-40'>
+                    <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path clipRule="evenodd" fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm0 8.625a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM15.375 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" />
+                    </svg>
+                  </div>
+                  <div className='h-6'>
+                    <button type="button" className="absolute w-[420px] -ml-[132px] text-white bg-purple-800  font-medium rounded-b-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Inscribirse</button>
+                  </div>
+                </div>
+              </div>
+              <div className='flex gap-4 bg-gradient-to-br from-purple-400  to-emerald-700 p-4 rounded-md w-[420px] items-center overflow-hidden '>
+                <div className='-mt-8'>
+                  <img src='https://i.imgur.com/FMP6aFy.png' width={100} height={100} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  < text className='font-bold text-xl' >
+                    Let's learn English
+                  </text >
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Dicatado por:
+                    </text>
+                    <text className='font-bold'>
+                      Jennipher Dolinsky
+                    </text>
+                  </span>
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Nivel:
+                    </text>
+                    <text className='font-bold'>
+                      Intermedio
+                    </text>
+                  </span>
+                  <span className=' flex gap-1 font-bold'>
+                    <text className=''>
+                      Sabado 12 de Junio
+                    </text>
+                    <text className=''>
+                      a las 7:00 pm
+                    </text>
+                  </span>
+                  <div className='absolute -ml-40'>
+                    <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path clipRule="evenodd" fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm0 8.625a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM15.375 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" />
+                    </svg>
+                  </div>
+                  <div className='h-6'>
+                    <button type="button" className="absolute w-[420px] -ml-[132px] text-white bg-purple-800  font-medium rounded-b-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Inscribirse</button>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+
+          </div>
+
+          <div className="flex flex-col gap-4 justify-center items-center">
+            <h2 className='font-semibold text-5xl mb-4 text-center'>Chats Prensenciales</h2>
+            <div className="flex flex-wrap justify-center items-center gap-6">
+
+              <div className='flex gap-4 bg-gradient-to-br from-purple-400  to-emerald-700 p-4 rounded-md w-[420px] items-center overflow-hidden '>
+                <div className='-mt-8'>
+                  <img src='https://i.imgur.com/FMP6aFy.png' width={100} height={100} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  < text className='font-bold text-xl' >
+                    Let's learn English
+                  </text >
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Dicatado por:
+                    </text>
+                    <text className='font-bold'>
+                      Jennipher Dolinsky
+                    </text>
+                  </span>
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Nivel:
+                    </text>
+                    <text className='font-bold'>
+                      Intermedio
+                    </text>
+                  </span>
+                  <span className=' flex gap-1 font-bold'>
+                    <text className=''>
+                      Sabado 12 de Junio
+                    </text>
+                    <text className=''>
+                      a las 7:00 pm
+                    </text>
+                  </span>
+                  <div className='absolute -ml-40'>
+                    <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path clipRule="evenodd" fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm0 8.625a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM15.375 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" />
+                    </svg>
+                  </div>
+                  <div className='h-6'>
+                    <button type="button" className="absolute w-[420px] -ml-[132px] text-white bg-purple-800  font-medium rounded-b-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Inscribirse</button>
+                  </div>
+                </div>
+              </div>
+              <div className='flex gap-4 bg-gradient-to-br from-purple-400  to-emerald-700 p-4 rounded-md w-[420px] items-center overflow-hidden '>
+                <div className='-mt-8'>
+                  <img src='https://i.imgur.com/FMP6aFy.png' width={100} height={100} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  < text className='font-bold text-xl' >
+                    Let's learn English
+                  </text >
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Dicatado por:
+                    </text>
+                    <text className='font-bold'>
+                      Jennipher Dolinsky
+                    </text>
+                  </span>
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Nivel:
+                    </text>
+                    <text className='font-bold'>
+                      Intermedio
+                    </text>
+                  </span>
+                  <span className=' flex gap-1 font-bold'>
+                    <text className=''>
+                      Sabado 12 de Junio
+                    </text>
+                    <text className=''>
+                      a las 7:00 pm
+                    </text>
+                  </span>
+                  <div className='absolute -ml-40'>
+                    <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path clipRule="evenodd" fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm0 8.625a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM15.375 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" />
+                    </svg>
+                  </div>
+                  <div className='h-6'>
+                    <button type="button" className="absolute w-[420px] -ml-[132px] text-white bg-purple-800  font-medium rounded-b-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Inscribirse</button>
+                  </div>
+                </div>
+              </div>
+              <div className='flex gap-4 bg-gradient-to-br from-purple-400  to-emerald-700 p-4 rounded-md w-[420px] items-center overflow-hidden '>
+                <div className='-mt-8'>
+                  <img src='https://i.imgur.com/FMP6aFy.png' width={100} height={100} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  < text className='font-bold text-xl' >
+                    Let's learn English
+                  </text >
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Dicatado por:
+                    </text>
+                    <text className='font-bold'>
+                      Jennipher Dolinsky
+                    </text>
+                  </span>
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Nivel:
+                    </text>
+                    <text className='font-bold'>
+                      Intermedio
+                    </text>
+                  </span>
+                  <span className=' flex gap-1 font-bold'>
+                    <text className=''>
+                      Sabado 12 de Junio
+                    </text>
+                    <text className=''>
+                      a las 7:00 pm
+                    </text>
+                  </span>
+                  <div className='absolute -ml-40'>
+                    <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path clipRule="evenodd" fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm0 8.625a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM15.375 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" />
+                    </svg>
+                  </div>
+                  <div className='h-6'>
+                    <button type="button" className="absolute w-[420px] -ml-[132px] text-white bg-purple-800  font-medium rounded-b-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Inscribirse</button>
+                  </div>
+                </div>
+              </div>
+              <div className='flex gap-4 bg-gradient-to-br from-purple-400  to-emerald-700 p-4 rounded-md w-[420px] items-center overflow-hidden '>
+                <div className='-mt-8'>
+                  <img src='https://i.imgur.com/FMP6aFy.png' width={100} height={100} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  < text className='font-bold text-xl' >
+                    Let's learn English
+                  </text >
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Dicatado por:
+                    </text>
+                    <text className='font-bold'>
+                      Jennipher Dolinsky
+                    </text>
+                  </span>
+                  <span className=' flex gap-1'>
+                    <text className=''>
+                      Nivel:
+                    </text>
+                    <text className='font-bold'>
+                      Intermedio
+                    </text>
+                  </span>
+                  <span className=' flex gap-1 font-bold'>
+                    <text className=''>
+                      Sabado 12 de Junio
+                    </text>
+                    <text className=''>
+                      a las 7:00 pm
+                    </text>
+                  </span>
+                  <div className='absolute -ml-40'>
+                    <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path clipRule="evenodd" fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm0 8.625a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM15.375 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" />
+                    </svg>
+                  </div>
+                  <div className='h-6'>
+                    <button type="button" className="absolute w-[420px] -ml-[132px] text-white bg-purple-800  font-medium rounded-b-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Inscribirse</button>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default page
