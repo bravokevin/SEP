d5e1a9910e0628b02bac98d4374a1a91b8ff26a6
Kevin Bravo <bravokevinto@gmail.com>
2023-06-03 19:09:45 -0400

    style: add responsive design

--- src/app/(public)/ofertaDeChats/page.tsx
+++ src/app/(public)/ofertaDeChats/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useSearchParams } from 'next/navigation'
 import ChatCard from '@/components/card/ChatCard'
 import { Chat } from '@/types/Chat'
+import Image from 'next/image'
 
 interface ChatQuery extends Chat {
   inscriptionLink: string
@@ -16,9 +17,9 @@ const page = () => {
   return (
     <main className='bg-purple-300 flex flex-col'>
       <div className='w-full border-b-8 border-purple-800 '>
-        <img src="https://i.imgur.com/oaJRKuz.png" alt="" className=' w-full h-[300px]' />
+        <Image src="https://i.imgur.com/oaJRKuz.png" alt="" width={1600} height={300} />
       </div>
-      <div className="bg-white w-[1400px] m-auto p-4 rounded-md mt-4 text-black text-center">
+      <div className="bg-white w-full m-auto p-4 rounded-md mt-4 text-black text-center">
         <h2 className=' text-3xl font-bold'>
           Planed Schedule for the month
         </h2>
@@ -29,8 +30,8 @@ const page = () => {
       <div className="p-8 lg:space-y-0 lg:pt-12 min-h-screen ">
         <div className="flex flex-col items-center gap-8">
           <div className="flex flex-col gap-4 justify-center items-center">
-            <h2 className='font-bold text-5xl mb-4 text-black bg-white w-[1400px] p-4 text-center rounded-md'>Chats Virtuales</h2>
-            <div className="flex flex-wrap justify-center items-center gap-6">
+            <h2 className='font-bold text-5xl mb-4 text-black bg-white w-full p-4 text-center rounded-md'>Chats Virtuales</h2>
+            <div className="flex flex-wrap justify-center items-center gap-6 p-4 sm:p-0">
               {f.map((chat, index: number) => {
                 const { workshop, completeFormUrl } = chat;
                 if (workshop.kindOfChat === 'Virtual') {
@@ -40,9 +41,9 @@ const page = () => {
             </div>
           </div>
           <div className="flex flex-col gap-4 justify-center items-center ">
-            <h2 className='font-bold text-5xl mb-4 text-black bg-white p-4 text-center w-[1400px] rounded-md'>Chats Presenciales</h2>
-            <div className="flex flex-wrap justify-center items-center gap-6">
-            {f.map((chat, index: number) => {
+            <h2 className='font-bold text-5xl mb-4 text-black bg-white p-4 text-center w-full rounded-md'>Chats Presenciales</h2>
+            <div className="flex flex-wrap justify-center items-center gap-6 p-4 sm:p-0">
+              {f.map((chat, index: number) => {
                 const { workshop, completeFormUrl } = chat;
                 if (workshop.kindOfChat === 'Presencial') {
                   return <ChatCard key={index} chatName={workshop.name} speaker={workshop.speaker} level={workshop.level} date={workshop.date} hour={workshop.startHour} inscriptionLink={completeFormUrl} />
